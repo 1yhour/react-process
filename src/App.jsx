735282826e0b500468a0dd6ba43f7e459296be88
LYHOUR SENG <lyhourlucky77@gmail.com>
2026-08-15 22:36:15 +0700

    feat: simple pizza menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,101 @@
-import { useEffect, useState } from "react";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-export default function Home(){
-  const [advice, setAdvice] = useState('');
-  const [count , setCount] = useState(0);
-  async function getAdvice(){
-    const res = await fetch('https://api.adviceslip.com/advice');
-    const data = await res.json();
-    setAdvice(data.slip.advice);  
-    setCount((c) => c+1);
-  }
-  useEffect(function(){
-    getAdvice();
-  },[])
-  return(
-    <div>
-      <h1>{advice}</h1>
-      <button onClick={getAdvice} className="border-2 rounded-2xl mt-20">Click me</button>
-      <Message count={count}/>
+export default function App() {
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      <Header />
+      <Menu
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        price={12}
+        photoName="pizzas/prosciutto.jpg"
+      />
+      <Menu
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        price={12}
+        photoName="pizzas/salamino.jpg"
+      />
+      
+      <Footer />
     </div>
-  )
+  );
 }
 
-function Message(props){
-  return(
-    <p>{props.count}</p>
-  )
+function Header() {
+  return (
+    <header className="header">
+      <h1>FAST REACT PIZZA CO.</h1>
+    </header>
+  );
+}
+function Menu(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 5}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  return (
+    <footer className="footer">
+      <p>
+        <span>It's {hour}</span>{" "}
+        {hour >= 12 && hour < 22
+          ? "We're currently open!"
+          : "We're currently closed!"}
+      </p>
+    </footer>
+  );
 }
